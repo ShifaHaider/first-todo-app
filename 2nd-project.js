ae26fdd3411c623f0106b28ed6e6c82b0a443cfa
Shifa Haider <shifa.haider065@gmail.com>
2018-01-24 14:04:32 +0500

    save new code

--- 2nd-project.js
+++ 2nd-project.js
@@ -1,51 +1,38 @@
 var inp = document.getElementById('inp');
 var list = document.getElementById('list');
-var todos = [];
+var todos = JSON.parse(localStorage.getItem('todos')) || [];
+render();
+var indexForEdit;
+function setValueForEdit(i){
+    console.log(i);
+    indexForEdit = i;
+    inp.value = list.children[i].childNodes[0].textContent;
+    console.log(list.children[i].childNodes[0]);
+}
+function doEdit(){
+    todos[indexForEdit]= inp.value;
+    localStorage.setItem('todos' , JSON.stringify(todos));
+    render();
+    indexForEdit = null;
+    inp.value = '';
+}
 function add (){
     todos.push(inp.value);
+    localStorage.setItem('todos' , JSON.stringify(todos));
     inp.value ='';
     render();
 }
-function toEdit(i){
-    inp.value = todos[i].children[0].childNodes[0].textContent;
-    render();
-}
+
 function render(){
     list.innerHTML ='';
     for (var i = 0; i < todos.length; i++){
         var li = document.createElement('li');
-        li.innerHTML = todos[i] + '<button onclick="edit('+ i +')">Edit</button> <button onclick="remove('+ i +')">Delete</button>';
+        li.innerHTML = todos[i] + '<button onclick="setValueForEdit('+ i +')">Edit</button> <button onclick="remove('+ i +')">Delete</button>';
         list.appendChild(li);
     }
 }
 function remove(i){
     todos.splice(i, 1);
+    localStorage.setItem('todos' , JSON.stringify(todos));
     render();
 }
-function edit(i){
-     render();
-     todos.push(inp.value = todos[i]);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function secEdit(i){
-//    render();
-//    edit(i);
-//    todos[i].innerHTML = inp.value;
-//}
-
-
-
